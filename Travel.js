7872261feab85ca599df9d8d44e652181f4d1bff
Csantana1121 <christians1121@gmail.com>
2021-11-30 10:50:55 -0600

    query api button added

---
+++ Travel.js
@@ -0,0 +1,21 @@
+var startButton = document.getElementById('travel');
+startButton.onclick = function()  {   
+  const data = null;
+
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = false;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+      var json = JSON.parse(this.response)
+      console.log(json['data'][0]['result_object']['photo']['images']['original']['url']);
+    }
+  });
+
+  xhr.open("GET", "https://travel-advisor.p.rapidapi.com/locations/search?query=eiffel%20tower&lang=en_US&units=km");
+  xhr.setRequestHeader("x-rapidapi-host", "travel-advisor.p.rapidapi.com");
+  xhr.setRequestHeader("x-rapidapi-key", "3b2fd2f1dbmsh5e3cd57f3775ac7p1cada7jsn3b71280bf6c5");
+
+  xhr.send(data);
+};
