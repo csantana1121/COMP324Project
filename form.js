b81375d20256444a6d8beb342f20db974e2bfb6e
Christian Santana <christians1121@gmail.com>
2021-11-30 19:37:18 -0600

    basic form that gets user input

---
+++ form.js
@@ -0,0 +1,11 @@
+var submit = document.getElementById("submit");
+submit.onclick = function()  {  
+    var formdata = new FormData();
+    var name = document.getElementById("name").value;
+    console.log(name);
+    var option = document.getElementsByName("recommed");
+    for(var i = 0; i < option.length; i++) {
+        if(option[i].checked)
+        console.log(option[i].value);
+    }
+};
