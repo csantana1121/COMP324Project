ce16e4cd05e22812b599ee133dcb218314c0cd60
Csantana1121 <christians1121@gmail.com>
2021-12-03 18:30:03 -0600

    made firebase db

--- form.js
+++ form.js
@@ -1,8 +1,10 @@
 var submit = document.getElementById("submit");
 submit.onclick = function()  {  
+    const db = firebase.database();
     var formdata = new FormData();
     var name = document.getElementById("name").value;
     console.log(name);
+    db.ref('q1/name').set(name);
     var option = document.getElementsByName("recommed");
     // document.querySelector('meta[name="name"]').setAttribute("content",name)
     // document.querySelector('meta[name="name"]').content = name;
@@ -11,6 +13,6 @@ submit.onclick = function()  {
         if(option[i].checked)
         console.log(option[i].value);
     }
-    window.location.href = "results.html";
+    // window.location.href = "results.html";
 
 };
