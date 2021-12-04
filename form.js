ed29c90b45155f9cfa8c98f54df57b76e3bd0f30
Csantana1121 <christians1121@gmail.com>
2021-12-04 13:09:36 -0600

    working version

--- form.js
+++ form.js
@@ -3,8 +3,10 @@ submit.onclick = function()  {
     const db = firebase.database();
     var formdata = new FormData();
     var name = document.getElementById("name").value;
+    var loc = document.getElementById("location").value;
     console.log(name);
-    db.ref('q1/name').set(name);
+    db.ref('results/q1').set(name);
+    db.ref('results/q3').set(loc);
     var option = document.getElementsByName("recommed");
     // document.querySelector('meta[name="name"]').setAttribute("content",name)
     // document.querySelector('meta[name="name"]').content = name;
@@ -12,7 +14,8 @@ submit.onclick = function()  {
     for(var i = 0; i < option.length; i++) {
         if(option[i].checked)
         console.log(option[i].value);
+        db.ref('results/q2').set(option[i].value);
     }
-    // window.location.href = "results.html";
+    window.location.href = "results.html";
 
 };
