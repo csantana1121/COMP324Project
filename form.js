6669beadf2a4cbf72e30e128c1548e1d18b7e041
Csantana1121 <christians1121@gmail.com>
2021-12-05 18:39:51 -0600

    linked to db

--- form.js
+++ form.js
@@ -5,18 +5,51 @@ function changeurl(){
 submit.onclick = function()  {  
     const db = firebase.database();
     var formdata = new FormData();
-    var name = document.getElementById("name").value;
-    var loc = document.getElementById("location").value;
-    console.log(name);
-    db.ref('results/q1').set(name);
-    var option = document.getElementsByName("recommed");
+    var nights = document.getElementById("NumOfNights").value;
+    // var loc = document.getElementById("location").value;
+    // console.log(name);
+    db.ref('results/q6').set(nights);
+    var selectElement = document.querySelector('#loc');
+    var output = selectElement.value;
+    var option = document.getElementsByName("budget");
+    console.log(output);
+    var option2 = document.getElementsByName("SpendtheNight");
+    var markedCheckbox = document.getElementsByName('food');
+    var arr = []
+    for (var checkbox of markedCheckbox) {  
+        if (checkbox.checked)  
+          arr.push(checkbox.value);
+      }  
+      db.ref('results/q2').set(arr);
+    var markedCheckbox2 = document.getElementsByName('adventure');
+    // console.log(markedCheckbox2)
+    var arr2 = []
+    for (var checkbox2 of markedCheckbox2) {  
+        if (checkbox2.checked){ 
+            console.log(checkbox2.value)
+          arr2.push(checkbox2.value);
+        }
+      }  
+      db.ref('results/q4').set(arr2);
+    markedCheckbox = document.getElementsByName('transport');
+    // console.log(markedCheckbox2)
+    arr = []
+    for (var checkbox3 of markedCheckbox) {  
+        if (checkbox3.checked){ 
+            console.log(checkbox3.value)
+          arr.push(checkbox3.value);
+        }
+      }  
+      db.ref('results/q7').set(arr);
+    var comment = document.getElementById("comment").value;
+    db.ref('results/q8').set(comment)
     // document.querySelector('meta[name="name"]').setAttribute("content",name)
     // document.querySelector('meta[name="name"]').content = name;
     // console.log(document.querySelector('meta[name="name"]').content)
     for(var i = 0; i < option.length; i++) {
         if(option[i].checked){
         console.log(option[i].value);
-        db.ref('results/q2').set(option[i].value).then(() => {
+        db.ref('results/q3').set(option[i].value).then(() => {
             // log data set success to console
             console.log('data set...');
             // window.open( "results.html");
@@ -27,7 +60,21 @@ submit.onclick = function()  {
             });
         }  
     }
-    db.ref('results/q3').set(loc).then(() => {
+    for(var i = 0; i < option2.length; i++) {
+        if(option2[i].checked){
+        console.log(option2[i].value);
+        db.ref('results/q5').set(option2[i].value).then(() => {
+            // log data set success to console
+            console.log('data set...');
+            // window.open( "results.html");
+            })
+            .catch((e) => {
+            // catcg error from Firebase - error logged to console
+            console.log('error returned', e);
+            });
+        }  
+    }
+    db.ref('results/q1').set(output).then(() => {
         // log data set success to console
         console.log('data set...');
         window.location.href = "results.html";
@@ -36,10 +83,4 @@ submit.onclick = function()  {
         // catcg error from Firebase - error logged to console
         console.log('error returned', e);
         });
-        // window.close();
-    // window.location.replace("results.html");    
-    // setTimeout(function () {
-    //     console.log("Hi 5 seconds laters");
-    // }, 5000);
-    // window.location.href = "results.html";
 };
--- geocoding.js
+++ geocoding.js
@@ -1,24 +1,7 @@
 function Append(){
     const data = null;
     const db = firebase.database();
-    // const xhr = new XMLHttpRequest();
-    // xhr.withCredentials = false;
-
-    // xhr.addEventListener("readystatechange", function () {
-    // if (this.readyState === this.DONE) {
-    //   console.log(this.responseText);
-    //   var json = JSON.parse(this.response)
-    //   console.log(json['data'][1]['result_object']['photo']['images']['large']['url']);
-    //   var output = document.getElementById("output");
-    // //   var $note ="<img src='https://media-cdn.tripadvisor.com/media/photo-o/1a/5b/4b/16/caption.jpg'><img>";
-    //   var query = "<div><img src='";
-    //   query += json['data'][0]['result_object']['photo']['images']['large']['url'];
-    //   query += "'></div>"
-    //   const parser = new DOMParser();
-    // //   console.log(parser.parseFromString($note, 'text/html').firstChild);
-    // //   output.append(parser.parseFromString($note, 'text/html').firstChild);
-    //   output.append(parser.parseFromString(query, 'text/html').firstChild);
-      db.ref('results/q3').once('value')
+      db.ref('results/q1').once('value')
         .then((snapshot) => {
         // snapshot of the data - request the return value for the data at the time of query...
         const data = snapshot.val();
@@ -28,17 +11,7 @@ function Append(){
         .catch((e) => {
         console.log('error returned - ', e);
         });
-    //   output.append(parser.parseFromString(json['data'][0]['result_object']['photo']['images']['original']['url']))
-    //   output.append("<img src='https://media-cdn.tripadvisor.com/media/photo-o/1a/5b/4b/16/caption.jpg'>");
     }
-//   });
-
-//   xhr.open("GET", "https://travel-advisor.p.rapidapi.com/locations/search?query=Chicago&lang=en_US&units=km");
-//   xhr.setRequestHeader("x-rapidapi-host", "travel-advisor.p.rapidapi.com");
-//   xhr.setRequestHeader("x-rapidapi-key", "3b2fd2f1dbmsh5e3cd57f3775ac7p1cada7jsn3b71280bf6c5");
-
-//   xhr.send(data);
-// }
 function GeoCode(location,store)  {   
     const data = null;
     const key = "AIzaSyBn3PMPpjblLpWGNw4GCq-cZoqb2SeYWc8";
